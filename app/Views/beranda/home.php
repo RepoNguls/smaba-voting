764955e79ec7ff6058494b848e53c6fbe768aabe
Kurniawan Ramadhani <dhannyjsb@gmail.com>
2021-09-10 17:25:59 +0800

    permudah

--- app/Views/beranda/home.php
+++ app/Views/beranda/home.php
@@ -5,7 +5,13 @@
         <h6 class="mb-0 text-uppercase">Selamat Datang</h6>
         <hr>
         Selamat datang <?= $user['nama']; ?> kelas <?= $user['kelas']; ?>
+        <hr>
+        <div class="d-grid gap-2">
+            <a href="<?php echo base_url(); ?>/user/kegiatan/pemilihan-osis" class="btn btn-dark radius-10" id="btn">Halaman Pemilihan OSIS</a>
+        </div>
     </div>
 </div>
+
+
 
 <!--end row-->
