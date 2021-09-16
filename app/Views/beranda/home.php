376ff4c9019f1cd8323424397e200622377c4f13
Kurniawan Ramadhani <dhannyjsb@gmail.com>
2021-09-16 08:11:19 +0800

    up up

--- app/Views/beranda/home.php
+++ app/Views/beranda/home.php
@@ -4,7 +4,7 @@
     <div class="card-body">
         <h6 class="mb-0 text-uppercase">Selamat Datang</h6>
         <hr>
-        Selamat datang <?= $user['nama']; ?> kelas <?= $user['kelas']; ?>
+        Selamat datang <?= $user['nama']; ?>, <?= $user['kelas']; ?>
         <hr>
         <div class="d-grid gap-2">
             <a href="<?php echo base_url(); ?>/user/kegiatan/pemilihan-osis" class="btn btn-dark radius-10" id="btn">Halaman Pemilihan OSIS</a>
